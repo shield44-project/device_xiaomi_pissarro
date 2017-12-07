67114888cd89cb292cf60b885967c258585d4737
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    pissarro: Build dummy android.hidl.[base,manager]@1.0
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c

--- device.mk
+++ device.mk
@@ -56,6 +56,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
