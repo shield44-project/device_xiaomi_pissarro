480feaa64009bb2e6f0377641c8ff404cec3a65d
Peter Cai <peter@typeblog.net>
2019-11-02 13:20:37 +0800

    pissarro: releasetools: remove fuseblk from system sepolicy
    * We basically need the same hack for every frickin MTK device
    
    Signed-off-by: theimpulson <aayushgupta219@gmail.com>
    Change-Id: I065dfc54355433c7aefdb7828cbe77ec0b25295e

--- releasetools.py
+++ releasetools.py
@@ -22,8 +22,15 @@ def AddImage(info, basename, dest):
     info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
+    PatchSELinux(info)
     info.script.Print("Patching firmware images...")
     AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
     AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
     AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
     return
+
+def PatchSELinux(info):
+  info.script.Print("Patching Duplicate SELinux Defines...")
+  info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/bootdevice/by-name/system", "/system_root");')
+  info.script.AppendExtra('run_program("/sbin/sed", "-i", "/fuseblk/d", "/system_root/system/etc/selinux/plat_sepolicy.cil");')
+  info.script.AppendExtra('unmount("/system_root");')
