7d9cb2e9656eebff91a9983155ff3978f6633085
maxwen <max.weninger@gmail.com>
2019-11-24 23:38:56 +0100

    pissarro: enable TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS
    to prevent partition changes during ab updates
    
    Change-Id: Iab772f6752a16d37485d528749fe8fb50060e86b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,6 +114,11 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_ODM := odm
 
+# tell update_engine to not change dynamic partition table during updates
+# needed since our qti_dynamic_partitions does not include
+# vendor and odm and we also dont want to AB update them
+TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS := true
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
