320d4ce2d5ca92e88b31371c260c665e91aa0205
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    pissarro: rro_overlays: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: If6a7a5eec116ea8d058d612f65ac434e36375246

--- device.mk
+++ device.mk
@@ -236,7 +236,8 @@ PRODUCT_PACKAGES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    hostapd
+    hostapd \
+    WifiOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
